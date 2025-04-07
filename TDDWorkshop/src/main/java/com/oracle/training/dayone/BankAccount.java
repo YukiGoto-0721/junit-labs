6fdef04058751b1c593f851bdf558ee7e2333b9c
YukiGoto-0721 <rs015034@yahoo.co.jp>
2025-04-07 20:53:48 +0900

    First Commit

---
+++ TDDWorkshop/src/main/java/com/oracle/training/dayone/BankAccount.java
@@ -0,0 +1,13 @@
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
--- TDDWorkshop/src/test/java/com/oracle/training/dayone/BankAccount.java
+++ TDDWorkshop/src/test/java/com/oracle/training/dayone/BankAccount.java
@@ -1,5 +1,11 @@
-package com.oracle.training.dayone;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class BankAccount {
-
+public class BankAccountTest {
+    
+    @Test
+    void testInitialBalance() {
+        BankAccount account = new BankAccount("12345", 1000.0);
+        assertEquals(1000.0, account.getBalance());  // This test will fail because BankAccount does not exist
+    }
 }
